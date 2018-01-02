f86d84022492ead0f8a00c0df5824365415fb0e2
Jessica Kerr <jessitron@gmail.com>
2018-01-02 10:55:50 -0600

    Override lint rule

--- test/typescriptEditing/resources/before/src/CodeThatUsesIt.ts
+++ test/typescriptEditing/resources/before/src/CodeThatUsesIt.ts
@@ -1,6 +1,7 @@
 
 import { HandlerContext, logger } from "@atomist/automation-client";
 
+// tslint:disable-next-line:no-namespace
 namespace InHere {
     export function giveMeYourContext(otherParams: any) {
         return "says me";
