f49968c828f11e8652cc891a629c42412f7f4327
Jessica Kerr <jessitron@gmail.com>
2018-01-20 08:02:23 -0800

    use async

--- src/dependencyVersion/FingerprintAutomationClientVersion.ts
+++ src/dependencyVersion/FingerprintAutomationClientVersion.ts
@@ -17,7 +17,7 @@ import * as stringify from "json-stringify-safe";
 
 import { Fingerprint, PushFingerprintWorld } from "./fingerprint";
 import * as _ from "lodash";
-import { fetchFileContents, RemoteFileLocation } from "./fetchOneFile";
+import { GitHubFileWorld, RemoteFileLocation } from "./fetchOneFile";
 import { adminChannel } from "../credentials";
 
 export const AutomationClientVersionFingerprintName = "automation-client-version";
@@ -41,69 +41,70 @@ export class FingerprintAutomationClientVersion implements HandleEvent<graphql.P
     @Secret(Secrets.OrgToken)
     public githubToken: string;
 
-    public handle(e: EventFired<graphql.PushForFingerprinting.Query>,
-                  ctx: HandlerContext, params: this): Promise<HandlerResult> {
+    public async handle(e: EventFired<graphql.PushForFingerprinting.Query>,
+                        ctx: HandlerContext, params: this): Promise<HandlerResult> {
         logger.info(`Incoming event is %s`, JSON.stringify(e.data, null, 2));
-
         const push = e.data.Push[0];
-        const repo = push.repo;
-        const afterSha = push.after.sha;
-        const beforeSha: string = _.get(push, "before.sha");
-        const token = params.githubToken;
-        const teamId = configuration.teamIds[0];
-        const provider = (repo.org && repo.org.provider) ? repo.org.provider : {
-            apiUrl: "https://api.github.com",
-            url: "https://github.com",
-        };
+        try {
+            const repo = push.repo;
+            const afterSha = push.after.sha;
+            const provider = providerFromRepo(repo);
 
-
-        // get the contents, and if that worked, then calculate a fingerprint and push it
-        const packageLock: RemoteFileLocation = {
-            owner: repo.owner,
-            name: repo.name,
-            baseUrl: provider.url,
-            apiUrl: provider.apiUrl,
-            path: "package-lock.json",
-        };
-        const contents = fetchFileContents(token, packageLock, afterSha);
-        const pushedFingerprint: Promise<Fingerprint> = contents.then(plj => {
+            // get the contents, and if that worked, then calculate a fingerprint and push it
+            const packageLock: RemoteFileLocation = {
+                owner: repo.owner,
+                name: repo.name,
+                baseUrl: provider.url,
+                apiUrl: provider.apiUrl,
+                path: "package-lock.json",
+            };
+            const plj = await GitHubFileWorld.fetchFileContents(params.githubToken, packageLock, afterSha);
             if (plj === 404) {
-                return Promise.resolve({ name: "lack of automation-client-version", sha: "" })
-            } else {
-                const version = calculateFingerprint(plj);
-                const fingerprint = {
-                    name: AutomationClientVersionFingerprintName, sha: version,
-                };
-                return PushFingerprintWorld.pushFingerprint({
-                    provider: provider.url,
-                    owner: repo.owner, repo: repo.name, sha: afterSha,
-                }, fingerprint)
-                    .then(() => fingerprint)
+                return {
+                    code: 0, message:
+                        `no ${AutomationClientVersionFingerprintName} on ${repo.owner}/${repo.name}#${afterSha}`,
+                }
             }
-        });
+            const fingerprint = {
+                name: AutomationClientVersionFingerprintName, sha: calculateFingerprint(plj),
+            };
+            await PushFingerprintWorld.pushFingerprint({
+                provider: provider.url,
+                owner: repo.owner, repo: repo.name, sha: afterSha,
+            }, fingerprint);
 
-        // notify people if that didn't work.
-        return pushedFingerprint
-            .then(fingerprint => ({
+            return {
                 code: 0, message:
                     `reported fingerprint ${stringify(fingerprint)} on ${repo.owner}/${repo.name}#${afterSha}`,
-            }), error => reportFailure(ctx, push, error))
+            };
+        } catch (error) {
+            return reportFailure(ctx, push, error);
+        }
     }
 }
 
-function reportFailure(ctx: HandlerContext, push: graphql.PushForFingerprinting.Push, error: Error) {
+function providerFromRepo(repo) {
+    return (repo.org && repo.org.provider) ? repo.org.provider : {
+        apiUrl: "https://api.github.com",
+        url: "https://github.com",
+    };
+}
+
+function reportFailure(ctx: HandlerContext,
+                       push: graphql.PushForFingerprinting.Push, error: Error): Promise<HandlerResult> {
     const report = `Failure fingerprinting ${push.repo.owner}/${push.repo.name}#${push.after.sha}
 
 ${error.message}
 `;
     return ctx.messageClient.addressChannels(report, adminChannel)
+        .then(() => ({ code: 1, error }))
 }
 
-export function calculateFingerprint(jpl: string): string | null {
+export function calculateFingerprint(jpl: string): string {
     const dependencyOfInterest = JSON.parse(jpl).dependencies["@atomist/automation-client"];
     if (dependencyOfInterest) {
         return dependencyOfInterest.version
     } else {
-        return null;
+        return "NONE";
     }
 }
