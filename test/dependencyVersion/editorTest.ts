3636dd8a9f93f240d541e3832284f6df5447b87b
Jessica Kerr <jessitron@gmail.com>
2018-01-28 12:56:24 -0600

    skip unimplemented tests

--- test/dependencyVersion/editorTest.ts
+++ test/dependencyVersion/editorTest.ts
@@ -4,12 +4,12 @@ import * as assert from "power-assert";
 
 describe("updating @atomist/automation-client dependencies", () => {
 
-    it("updates package.json to the new version", done => {
+    it.skip("updates package.json to the new version", done => {
 
 
     });
 
-    it("runs npm install to update package-lock.json", done => {
+    it.skip("runs npm install to update package-lock.json", done => {
 
     })
 
